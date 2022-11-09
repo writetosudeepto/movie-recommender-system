eab62134ee8e0dea9f111a6e60a2b6b726bd66e0
writetosudeepto <writetosudeepto@gmail.com>
2022-11-10 03:54:16 +0530

    first deployment

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import streamlit as st
 import pickle
+import requests
 
 movies_list_df = pickle.load(open('movies.pkl', 'rb'))
 movies_list = movies_list_df['title'].values
 
 similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+
+def fetch_poster(movie_id):
+    response = requests.get(
+        'https://api.themoviedb.org/3/movie/{}?api_key=a4f63e73b7e46728c9f420e685aacb94&language=en-US'.format(
+            movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 
 def recommend(movie):
@@ -13,21 +22,40 @@ def recommend(movie):
     movies_list_rec = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
+    recommended_movies_posters = []
     for mv in movies_list_rec:
-        movie_id = mv[0]
+        movie_id = movies_list_df.iloc[mv[0]].movie_id
         recommended_movies.append(movies_list_df.iloc[mv[0]].title)
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_posters
 
-    return recommended_movies
 
-
-st.title('Movie Recommender System')
+st.title('Movie Recommending System')
 
 selected_movie_name = st.selectbox(
-    'How would you like to be contacted?',
+    'Which kind of movie would you like?',
     movies_list
 )
 
 if st.button('Recommend'):
-    recommendations = recommend(selected_movie_name)
-    for i in recommendations:
-        st.write(i)
+    names, posters = recommend(selected_movie_name)
+    col1, col2, col3, col4, col5 = st.   columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
+
+
+
+
