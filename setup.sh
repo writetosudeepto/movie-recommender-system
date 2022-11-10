c3910bfe7ddcb1269b351f0b31b7b9b71f857a7c
writetosudeepto <writetosudeepto@gmail.com>
2022-11-11 00:50:13 +0530

    make it better

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 mkdir -p ~/.streamlit/
-
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+# shellcheck disable=SC2028
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
